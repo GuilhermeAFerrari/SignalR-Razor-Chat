759e3d86e14b37a3a0b623517c216a72a267a912
Guilherme Ferrari <guile.ferrari@hotmail.com>
2024-09-06 23:16:24 -0300

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalRRazorPages.Pages;
+
+public class IndexModel : PageModel
+{
+    public IndexModel()
+    {
+    }
+
+    public void OnGet()
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapHub<ChatHub>("/chatHub"); // Mapeia o hub SignalR
+
+app.Run();
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
